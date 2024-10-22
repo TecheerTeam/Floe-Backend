147f7e7cf5ed5803f9062a1e988b132c7cf74306
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-10-22 21:50:32 +0900

    chore: 스프링부트 세팅

---
+++ floe/src/main/java/project/floe/FloeApplication.java
@@ -0,0 +1,13 @@
+package project.floe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloeApplication.class, args);
+	}
+
+}
---
+++ floe/src/test/java/project/floe/FloeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.floe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
