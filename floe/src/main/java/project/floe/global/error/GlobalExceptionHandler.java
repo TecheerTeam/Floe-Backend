9c839139798a64f438d6875fd1cafdbf53fd5730
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 16:51:52 +0900

    feat: 예외처리를 위한 핸들러 및 에러응답 세팅

---
+++ floe/src/main/java/project/floe/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package project.floe.global.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.floe.global.error.exception.BusinessException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler // @RestController에서 발생한 에러 핸들러, @Controller 관련 어노테이션이 있어야지만 사용 가능
+    protected ResponseEntity<ErrorResponse> handleRuntimeException(BusinessException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response =
+                ErrorResponse.builder()
+                        .errorMessage(errorCode.getMessage())
+                        .businessCode(errorCode.getCode())
+                        .build();
+        log.warn(e.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(response);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INPUT_INVALID_VALUE, e.getBindingResult());
+        log.warn(e.getMessage());
+        return ResponseEntity.status(ErrorCode.INPUT_INVALID_VALUE.getStatus()).body(response);
+    }
+}
