7c05f255a210d1c8b415e68e1acad5ed84b898c2
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 20:41:15 +0900

    feat: RecordService 구현

--- floe/src/main/java/project/floe/domain/record/service/RecordService.java
+++ floe/src/main/java/project/floe/domain/record/service/RecordService.java
@@ -1,4 +1,19 @@
 package project.floe.domain.record.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.floe.domain.record.entity.Record;
+import project.floe.domain.record.repository.RecordJpaRepository;
+
+@Service
+@RequiredArgsConstructor
 public class RecordService {
+
+    private final RecordJpaRepository repository;
+
+    @Transactional
+    public Long save(Record newRecord) {
+        return repository.save(newRecord).getId();
+    }
 }
