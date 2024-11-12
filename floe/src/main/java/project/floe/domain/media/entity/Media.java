534767624f4d0fd4d4f15a73cb4ab6076d2b375f
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 15:41:16 +0900

    feat: Media Entity 생성

---
+++ floe/src/main/java/project/floe/domain/media/entity/Media.java
@@ -0,0 +1,34 @@
+package project.floe.domain.media.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드를 받는 생성자
+@Builder // 빌더 패턴 사용
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "media_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id", nullable = false)
+    private Record record;
+
+    @Column(name = "media_url", nullable = false)
+    private String mediaUrl;
+}
