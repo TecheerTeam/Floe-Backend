212be13dc2dd7b4b3039ed1dcfb28ed1e31a5006
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 20:16:37 +0900

    fix: Repository 오타 수정

--- floe/src/main/java/project/floe/domain/record/repository/RecordJpaRepository.java
+++ floe/src/main/java/project/floe/domain/record/repository/RecordJpaRepository.java
@@ -1,6 +1,9 @@
 package project.floe.domain.record.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.floe.domain.record.entity.Record;
 
-public interface RecordJpaRepository extends JpaRepository<Long, Record> {
+@Repository
+public interface RecordJpaRepository extends JpaRepository<Record, Long> {
 }
