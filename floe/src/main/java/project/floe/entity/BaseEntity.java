6d5e3ac99b8199e582d68ba2a4699f4e46984888
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 14:00:47 +0900

    feat: BaseEntity에 추가적인 설정 추가

--- floe/src/main/java/project/floe/entity/BaseEntity.java
+++ floe/src/main/java/project/floe/entity/BaseEntity.java
@@ -7,14 +7,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
+@ToString
 public abstract class BaseEntity {
 
     @CreatedDate
