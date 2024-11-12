ffa92bc58df43dfda0a1117c19ce53d380832ded
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-12 15:37:42 +0900

    feat: Record 객체 생성

--- floe/src/main/java/project/floe/domain/record/entity/Record.java
+++ floe/src/main/java/project/floe/domain/record/entity/Record.java
@@ -1,4 +1,51 @@
 package project.floe.domain.record.entity;
 
-public class Record {
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.floe.domain.media.entity.Media;
+import project.floe.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Record extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id")
+    private Long id;
+
+    /*
+    게시글 작성자의 정보를 담는 필드
+    추후 User개발 완료시 해당 코드로 변경
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    */
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    private String title;
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private RecordType recordType;
+
+    @OneToMany(mappedBy = "record", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Media> medias;
 }
