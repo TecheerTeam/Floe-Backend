0ed6b309ea7be44ef977f5af21405feca56dd91c
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-13 16:35:23 +0900

    feat: RecordController createRecord 구현 완료

--- floe/src/main/java/project/floe/domain/record/controller/RecordController.java
+++ floe/src/main/java/project/floe/domain/record/controller/RecordController.java
@@ -4,18 +4,12 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import project.floe.domain.media.entity.Media;
-import project.floe.domain.media.service.MediaService;
+import project.floe.domain.record.service.MediaService;
 import project.floe.domain.record.dto.request.CreateRecordRequest;
 import project.floe.domain.record.dto.response.CreateRecordResponse;
 import project.floe.domain.record.entity.Record;
@@ -34,15 +28,14 @@ public class RecordController {
     @PostMapping
     public ResponseEntity<ResultResponse> createRecord(@RequestPart(value = "dto") CreateRecordRequest dto,
                                                        @RequestPart(value = "files") List<MultipartFile> files) {
-        List<Media> medias = mediaService.uploadFiles(files);
         Record newRecord = Record.builder()
                 .userId(dto.getUserId())
                 .title(dto.getTitle())
                 .content(dto.getContent())
                 .recordType(dto.getRecordType())
-                .medias(medias)
                 .build();
-        CreateRecordResponse response = CreateRecordResponse.from(recordService.save(newRecord));
+        CreateRecordResponse response = CreateRecordResponse.from(recordService.save(newRecord, files));
+
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResultResponse.of(ResultCode.RECORD_CREATE_SUCCESS, response));
     }
