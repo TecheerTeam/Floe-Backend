f15d5968db3e0df5af8465383b6884ee7d888969
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-13 18:26:28 +0900

    refactor: 리포지토리 변수명 수정

--- floe/src/main/java/project/floe/domain/record/service/RecordService.java
+++ floe/src/main/java/project/floe/domain/record/service/RecordService.java
@@ -7,18 +7,30 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import project.floe.domain.record.entity.Record;
 import project.floe.domain.record.repository.RecordJpaRepository;
+import project.floe.global.error.ErrorCode;
+import project.floe.global.error.exception.EmptyResultException;
 
 @Service
 @RequiredArgsConstructor
 public class RecordService {
 
     private final MediaService mediaService;
-    private final RecordJpaRepository repository;
+    private final RecordJpaRepository recordRepository;
 
     @Transactional
     public Long save(Record record, List<MultipartFile> files) {
-        Record savedRecord = repository.save(record);
+        Record savedRecord = recordRepository.save(record);
         mediaService.uploadFiles(savedRecord, files);
         return savedRecord.getId();
+    }
+
+    @Transactional
+    public void deleteRecord(Long recordId) {
+        recordRepository.deleteById(recordId);
+    }
+
+    public Record findRecordById(Long recordId){
+        return recordRepository.findById(recordId)
+                .orElseThrow(() -> new EmptyResultException(ErrorCode.RECORD_NOT_FOUND_ERROR));
     }
 }
