8578b6e976dabc497d9eb696dadba9b74ef91394
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-13 19:13:34 +0900

    feat: 개별 조회 api 구현

--- floe/src/main/java/project/floe/domain/record/controller/RecordController.java
+++ floe/src/main/java/project/floe/domain/record/controller/RecordController.java
@@ -1,12 +1,14 @@
 package project.floe.domain.record.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import project.floe.domain.record.dto.request.CreateRecordRequest;
 import project.floe.domain.record.dto.response.CreateRecordResponse;
+import project.floe.domain.record.dto.response.GetDetailRecordResponse;
+import project.floe.domain.record.dto.response.MediaResponse;
 import project.floe.domain.record.entity.Record;
 import project.floe.domain.record.service.RecordService;
 import project.floe.global.result.ResultCode;
@@ -40,6 +44,26 @@ public class RecordController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResultResponse.of(ResultCode.RECORD_CREATE_SUCCESS, response));
+    }
+
+    @GetMapping("/{recordId}")
+    public ResponseEntity<ResultResponse> getDetailRecord(@PathVariable("recordId") Long recordId){
+        Record findRecord = recordService.findRecordById(recordId);
+        GetDetailRecordResponse response = GetDetailRecordResponse.builder()
+                .userId(findRecord.getUserId())
+                .title(findRecord.getTitle())
+                .content(findRecord.getContent())
+                .recordType(findRecord.getRecordType())
+                .medias(findRecord.getMedias().stream()
+                        .map(media -> MediaResponse.builder()
+                                .mediaId(media.getId())
+                                .mediaUrl(media.getMediaUrl())
+                                .build()
+                        ).collect(Collectors.toList())
+                )
+                .build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ResultResponse.of(ResultCode.DETAIL_RECORD_GET_SUCCESS, response));
     }
 
     @DeleteMapping("/{recordId}")
