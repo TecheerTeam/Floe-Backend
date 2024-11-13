50d09ddd77ea1c743b21e808a58c717a72ec4cfe
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-13 16:34:42 +0900

    feat: RecordService save메서드 구현

--- floe/src/main/java/project/floe/domain/record/service/RecordService.java
+++ floe/src/main/java/project/floe/domain/record/service/RecordService.java
@@ -1,8 +1,10 @@
 package project.floe.domain.record.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import project.floe.domain.record.entity.Record;
 import project.floe.domain.record.repository.RecordJpaRepository;
 
@@ -10,10 +12,13 @@ import project.floe.domain.record.repository.RecordJpaRepository;
 @RequiredArgsConstructor
 public class RecordService {
 
+    private final MediaService mediaService;
     private final RecordJpaRepository repository;
 
     @Transactional
-    public Long save(Record newRecord) {
-        return repository.save(newRecord).getId();
+    public Long save(Record record, List<MultipartFile> files) {
+        Record savedRecord = repository.save(record);
+        mediaService.uploadFiles(savedRecord, files);
+        return savedRecord.getId();
     }
 }
