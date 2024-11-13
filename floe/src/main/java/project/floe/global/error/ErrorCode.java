4d5d1a513156e72979af9fecd5fc97640829b563
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-13 16:15:03 +0900

    feat: Media ErrorCode 추가

--- floe/src/main/java/project/floe/global/error/ErrorCode.java
+++ floe/src/main/java/project/floe/global/error/ErrorCode.java
@@ -17,6 +17,12 @@ public enum ErrorCode {
     INPUT_INVALID_VALUE(409, "G002", "잘못된 입력"),
     ACCESS_INVALID_VALUE(400, "G003", "잘못된 접근"),
 
+    // Media
+    S3_UPLOAD_FAIL_ERROR(500, "M001", "파일을 S3에 업로드할 수 없음"),
+    FILE_NAME_NOT_FOUND_ERROR(400, "M002", "파일 이름을 찾을 수 없음"),
+    CANNOT_CONVERT_TO_BYTE(500, "M003", "파일을 바이트 배열로 변환할 수 없음"),
+    CANNOT_SET_METADATA(500, "M004", "파일의 메타데이터를 설정할 수 없음"),
+
     // Record
     RECORD_NOT_FOUND_ERROR(400, "R001", "기록을 찾을 수 없음");
 
