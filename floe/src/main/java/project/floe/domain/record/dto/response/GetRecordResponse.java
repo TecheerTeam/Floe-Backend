a148ff65ef0b629299e5e45b39fb39ab038c8247
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-18 13:02:33 +0900

    feat: 전체 조회 할 때 사용할 dto 제작

---
+++ floe/src/main/java/project/floe/domain/record/dto/response/GetRecordResponse.java
@@ -0,0 +1,48 @@
+package project.floe.domain.record.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import project.floe.domain.record.entity.Record;
+import project.floe.domain.record.entity.RecordType;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GetRecordResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private RecordType recordType;
+
+    private List<MediaResponse> medias;
+
+    private List<String> tags;
+
+    private LocalDateTime createdAt;
+
+    public static GetRecordResponse from(Record record){
+        return GetRecordResponse.builder()
+                .id(record.getId())
+                .title(record.getTitle())
+                .content(record.getContent())
+                .recordType(record.getRecordType())
+                .medias(MediaResponse.from(record))
+                .tags(record.getTagNames())
+                .createdAt(record.getCreatedAt())
+                .build();
+    }
+
+    public static Page<GetRecordResponse> listOf(Page<Record> records) {
+        return records.map(GetRecordResponse::from);
+    }
+}
