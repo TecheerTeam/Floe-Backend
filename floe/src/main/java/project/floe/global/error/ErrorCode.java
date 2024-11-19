65fa40eabb40cfbc5d0e7ff4ed960fd4d2874bb5
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-19 12:40:17 +0900

    feat: ErrorCode 케이스 추가

--- floe/src/main/java/project/floe/global/error/ErrorCode.java
+++ floe/src/main/java/project/floe/global/error/ErrorCode.java
@@ -21,9 +21,10 @@ public enum ErrorCode {
     S3_UPLOAD_FAIL_ERROR(500, "M001", "파일을 S3에 업로드할 수 없음"),
     FILE_NAME_NOT_FOUND_ERROR(400, "M002", "파일 이름을 찾을 수 없음"),
     CANNOT_CONVERT_TO_BYTE(500, "M003", "파일을 바이트 배열로 변환할 수 없음"),
-
+    MEDIA_NOT_FOUND_ERROR(400,"M004", "미디어 파일을 찾을 수 없음"),
+    UPDATE_FILE_SEQUENCE_MISMATCH(400, "M005", "업데이트된 파일 순서와, 넘겨준 파일의 위치가 맞지 않음"),
     // Record
-    RECORD_NOT_FOUND_ERROR(400, "R001", "기록을 찾을 수 없음");
+    RECORD_NOT_FOUND_ERROR(400, "R001", "기록을 찾을 수 없음"), ;
 
     private final int status;
     private final String code;
