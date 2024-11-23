95cc80c56d3e81ee8b833f606f0737a5b95e39ed
박진형 <skill1218@naver.com>
2024-11-23 18:36:36 +0900

    feat: @Repository 추가

--- floe/src/main/java/project/floe/domain/user/repository/UserRepository.java
+++ floe/src/main/java/project/floe/domain/user/repository/UserRepository.java
@@ -2,9 +2,10 @@ package project.floe.domain.user.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.floe.domain.user.entity.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(String userId);
