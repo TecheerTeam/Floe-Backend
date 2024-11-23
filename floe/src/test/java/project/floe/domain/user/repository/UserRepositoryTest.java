d0ed532566dc6e38220fef0461542f813601c515
박진형 <skill1218@naver.com>
2024-11-23 14:51:33 +0900

    refactor: 사용하지 않는 import 및 코드 제거

--- floe/src/test/java/project/floe/domain/user/repository/UserRepositoryTest.java
+++ floe/src/test/java/project/floe/domain/user/repository/UserRepositoryTest.java
@@ -2,9 +2,7 @@ package project.floe.domain.user.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import jakarta.persistence.EntityManager;
 import java.util.Optional;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,11 +17,9 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private EntityManager entityManager;
 
     @Test
-    public void 유저조회성공_ByUserId(){
+    public void 유저조회성공_ByUserId() {
         User user = user();
         userRepository.save(user);
 
@@ -35,7 +31,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void 유저조회실패_존재하지않는아이디(){
+    public void 유저조회실패_존재하지않는아이디() {
         User user = user();
         userRepository.save(user);
 
@@ -46,7 +42,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void 유저생성시_아이디중복확인(){
+    public void 유저생성시_아이디중복확인() {
         User user = user();
         userRepository.save(user);
 
@@ -58,7 +54,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void 유저생성시_이메일중복확인(){
+    public void 유저생성시_이메일중복확인() {
         User user = user();
         userRepository.save(user);
 
@@ -70,7 +66,7 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void 유저생성성공(){
+    public void 유저생성성공() {
         User user = user();
 
         User savedUser = userRepository.save(user);
@@ -80,18 +76,18 @@ public class UserRepositoryTest {
         assertThat(savedUser.getUserId()).isEqualTo(user.getUserId());
     }
 
-   @Test
-   public void 유저삭제성공(){
-       User user = user();
-       userRepository.save(user);
+    @Test
+    public void 유저삭제성공() {
+        User user = user();
+        userRepository.save(user);
 
-       userRepository.delete(user);
+        userRepository.delete(user);
 
-       Optional<User> findUser = userRepository.findByUserId(user.getUserId());
-       assertThat(findUser).isEmpty();
-   }
+        Optional<User> findUser = userRepository.findByUserId(user.getUserId());
+        assertThat(findUser).isEmpty();
+    }
 
-    private User user(){
-        return new User(null,"role","userId","password","name","email",1,20,"image","field");
+    private User user() {
+        return new User(null, "role", "userId", "password", "name", "email", 1, 20, "image", "field");
     }
 }
