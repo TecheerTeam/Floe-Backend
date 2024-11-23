adb2a2bdff16527df88639de60791c8a42e94f9f
박진형 <skill1218@naver.com>
2024-11-23 14:13:12 +0900

    feat: User 객체 필드 notnull 처리

--- floe/src/main/java/project/floe/domain/user/entity/User.java
+++ floe/src/main/java/project/floe/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package project.floe.domain.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,14 +25,31 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String role;
+
+    @Column(nullable = false)
     private String userId;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private int experience;
+
+    @Column(nullable = false)
     private int age;
+
     private String profileImage;
+
+    @Column(nullable = false)
     private String field;
 
     public User(SignUpRequestDto dto){
