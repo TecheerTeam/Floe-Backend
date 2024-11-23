ac9c83eda60ca65a72e0c5997285c975bdebecd7
박진형 <skill1218@naver.com>
2024-11-23 14:23:00 +0900

    feat: SignUpRequestDto @Validation 추가

--- floe/src/main/java/project/floe/domain/user/dto/request/SignUpRequestDto.java
+++ floe/src/main/java/project/floe/domain/user/dto/request/SignUpRequestDto.java
@@ -1,5 +1,7 @@
 package project.floe.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,13 +10,27 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class SignUpRequestDto {
+    @NotBlank(message = "User userId cannot be blank")
     private String userId;
+
+    @NotBlank(message = "User password cannot be blank")
     private String password;
+
+    @NotBlank(message = "User name cannot be blank")
     private String name;
+
+    @NotBlank(message = "User email cannot be blank")
     private String email;
-    private int experience;
-    private int age;
+
+    @NotNull(message = "User experience cannot be blank")
+    private Integer experience;
+
+    @NotNull(message = "User age cannot be null")
+    private Integer age;
+
     private String profileImage;
+
+    @NotBlank(message = "User field cannot be blank")
     private String field;
 
 }
