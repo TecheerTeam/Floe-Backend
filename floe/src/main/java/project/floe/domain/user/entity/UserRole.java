14704d405f1825a00686dd36679f75612490565b
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-28 04:01:31 +0900

    refacotr: 코드 정렬

--- floe/src/main/java/project/floe/domain/user/entity/UserRole.java
+++ floe/src/main/java/project/floe/domain/user/entity/UserRole.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-   GUEST("ROLE_GUEST") , USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+    GUEST("ROLE_GUEST"), USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
     //스프링 시큐리티에서는 권한(Role) 코드에 항상 "ROLE_" 접두사가 앞에 붙어야 하기 떄문
     private final String key;
--- floe/src/main/java/project/floe/domain/user/repository/UserRepository.java
+++ floe/src/main/java/project/floe/domain/user/repository/UserRepository.java
@@ -9,7 +9,9 @@ import project.floe.domain.user.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+
     Optional<User> findByNickname(String nickname);
+
     Optional<User> findByRefreshToken(String refreshToken);
 
     /**
@@ -17,6 +19,6 @@ public interface UserRepository extends JpaRepository<User, Integer> {
      * 소셜 로그인으로 정보 제공을 동의한 순건 DB에 저장해야 하나, 아직 추가 정보(분야, 연차 등)을 입력받지 못함
      * 유저 객체는 DB에 있지만 추가 정보가 빠진 상태
      * 따라서 추가 정보를 입력받아 회원 가입을 진행할 때 소셜 타입, 식별자로 해당 회원 찾기 위한 메소드
-    **/
+     **/
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
 }
