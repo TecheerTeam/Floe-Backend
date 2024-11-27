40f981737a012b03e9109c2eb3ff503e8d28bbe1
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-28 04:35:32 +0900

    fix: 잘못된 조인 필드명 설정 수정

--- floe/src/main/java/project/floe/domain/record_like/entity/RecordLike.java
+++ floe/src/main/java/project/floe/domain/record_like/entity/RecordLike.java
@@ -33,7 +33,7 @@ public class RecordLike {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id" , referencedColumnName = "id")
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
