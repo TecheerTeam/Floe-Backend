338ad59f707000491429b349eabc2e67b8cae68a
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-28 04:02:00 +0900

    feat: 응답 코드 추가

--- floe/src/main/java/project/floe/global/error/ErrorCode.java
+++ floe/src/main/java/project/floe/global/error/ErrorCode.java
@@ -1,6 +1,7 @@
 package project.floe.global.error;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -32,7 +33,10 @@ public enum ErrorCode {
     USER_ID_DUPLICATION_ERROR(400,"U002","중복된 아이디"),
     USER_EMAIL_DUPLICATION_ERROR(400,"U003","중복된 이메일"),
     USER_NICKNAME_DUPLICATION_ERROR(400, "U004", "중복된 닉네임" ),
-    EMAIL_NOT_FOUND_ERROR(400, "U005", "해당 이메일이 존재하지 않음");
+    EMAIL_NOT_FOUND_ERROR(400, "U005", "해당 이메일이 존재하지 않음"),
+
+    //Auth
+    TOKEN_ACCESS_NOT_EXIST(401, "A001","토큰을 찾을 수 없음" ),;
 
     private final int status;
     private final String code;
--- floe/src/main/java/project/floe/global/result/ResultCode.java
+++ floe/src/main/java/project/floe/global/result/ResultCode.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
-/** {행위}_{목적어}_{성공여부} message 는 동사 명사형으로 마무리 */
+/**
+ * {행위}_{목적어}_{성공여부} message 는 동사 명사형으로 마무리
+ */
 
 @Getter
 @AllArgsConstructor
@@ -13,16 +15,18 @@ public enum ResultCode {
     // Record
     RECORD_CREATE_SUCCESS("R001", "기록 생성 성공"),
     RECORD_DELETE_SUCCESS("R002", "기록 제거 성공"),
-    DETAIL_RECORD_GET_SUCCESS("R003","개별 기록 조회 성공" ),
-    Record_PAGING_GET_SUCCESS("R004","페이징된 전체 기록 조회 성공" ),
+    DETAIL_RECORD_GET_SUCCESS("R003", "개별 기록 조회 성공"),
+    RECORD_PAGING_GET_SUCCESS("R004", "페이징된 전체 기록 조회 성공"),
     RECORD_MODIFY_SUCCESS("R005", "기록 수정 성공"),
 
     // User
-    USER_CREATE_SUCCESS("U001","유저 생성 성공"),
-    USER_GET_SUCCESS("U002","유저 조회 성공"),
-    USER_DELETE_SUCCESS("U003","유저 삭제 성공"),
-    USER_UPDATE_SUCCESS("U004","유저정보 수정 성공"),
-    ;
+    USER_CREATE_SUCCESS("U001", "유저 생성 성공"),
+    USER_GET_SUCCESS("U002", "유저 조회 성공"),
+    USER_DELETE_SUCCESS("U003", "유저 삭제 성공"),
+    USER_UPDATE_SUCCESS("U004", "유저정보 수정 성공"),
+    USER_OAUTH_SIGNUP_SUCCESS("U005", "소셜 로그인 유저 회원가입 성공"),
+    USER_LOGIN_FAIL("U006", "로그인 실패"),
+    USER_PROFILE_IMAGE_UPDATE_SUCCESS("U007", "프로필 사진 업데이트 성공");
 
     private final String code;
     private final String message;
