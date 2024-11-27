d089ec2a831e7e2ef3a03888416a953d8123fa02
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-11-28 04:00:11 +0900

    feat: User관련 컨트롤러 정리 및 기능 추가

--- floe/src/main/java/project/floe/domain/user/controller/UserController.java
+++ floe/src/main/java/project/floe/domain/user/controller/UserController.java
@@ -1,16 +1,22 @@
 package project.floe.domain.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import project.floe.domain.user.dto.request.UserSignUpRequest;
 import project.floe.domain.user.dto.request.UserUpdateRequest;
 import project.floe.domain.user.dto.response.GetUserResponseDto;
@@ -27,40 +33,41 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/{userId}")
+    // 회원 정보 조회
+    @GetMapping
     ResponseEntity<ResultResponse> getUser(
-            @PathVariable("userId") String userId
+            HttpServletRequest request
     ) {
-        GetUserResponseDto data = userService.getUser(userId);
+        GetUserResponseDto data = userService.getUser(request);
         ResultResponse response = ResultResponse.of(ResultCode.USER_GET_SUCCESS, data);
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping
-    ResponseEntity<ResultResponse> signUp(
-            @RequestBody UserSignUpRequest dto
-    ) {
-        userService.signUp(dto);
-        ResultResponse response = ResultResponse.of(ResultCode.USER_CREATE_SUCCESS);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @DeleteMapping("/{userId}")
-    ResponseEntity<ResultResponse> deleteUser(
-        @PathVariable("userId") String userId
-    ){
-        userService.deleteUser(userId);
+    // 회원 삭제
+    @DeleteMapping
+    ResponseEntity<ResultResponse> deleteUser(HttpServletRequest request) {
+        userService.deleteUser(request);
         ResultResponse response = ResultResponse.of(ResultCode.USER_DELETE_SUCCESS);
         return ResponseEntity.ok().body(response);
     }
 
-    @PatchMapping("/{userId}")
+    // 회원 정보 수정
+    @PatchMapping("/update")
     ResponseEntity<ResultResponse> updateUser(
-            @PathVariable("userId") String userId,
+            HttpServletRequest request,
             @RequestBody UserUpdateRequest dto
-            ){
-        UpdateUserResponseDto responseDto = userService.updateUser(userId, dto);
-        ResultResponse response = ResultResponse.of(ResultCode.USER_UPDATE_SUCCESS,responseDto);
+    ) {
+        UpdateUserResponseDto responseDto = userService.updateUser(request, dto);
+        ResultResponse response = ResultResponse.of(ResultCode.USER_UPDATE_SUCCESS, responseDto);
         return ResponseEntity.ok().body(response);
+    }
+
+    // 회원 프로필 사진 수정
+    @PutMapping("/profile")
+    public ResponseEntity<ResultResponse> updateProfile(
+            HttpServletRequest request,
+            @RequestPart("profileImage") MultipartFile profileImage) {
+        userService.updateProfileImage(request, profileImage);
+        return ResponseEntity.ok().body(ResultResponse.of(ResultCode.USER_PROFILE_IMAGE_UPDATE_SUCCESS));
     }
 }
