7beff4add0128fd8653b23fe3368acaaec89d84c
kyungsoo <yksoo00@naver.com>
2024-11-28 23:19:54 +0900

    feat(ErrorCode): 댓글 에러 코드 추가

--- floe/src/main/java/project/floe/global/error/ErrorCode.java
+++ floe/src/main/java/project/floe/global/error/ErrorCode.java
@@ -4,7 +4,9 @@ package project.floe.global.error;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-/** {주체}_{이유} message 는 동사 명사형으로 마무리 */
+/**
+ * {주체}_{이유} message 는 동사 명사형으로 마무리
+ */
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
@@ -15,8 +17,14 @@ public enum ErrorCode {
     INPUT_INVALID_VALUE(409, "G002", "잘못된 입력"),
     ACCESS_INVALID_VALUE(400, "G003", "잘못된 접근"),
 
-    // Record
-    RECORD_NOT_FOUND_ERROR(400,"R001","기록을 찾을 수 없음");
+    // Comment
+    COMMENT_PARENT_NOT_FOUND_ERROR(404, "C002", "부모 댓글을 찾을 수 없음"),
+    COMMENT_CANNOT_REPLY_TO_DELETED(409, "C003", "삭제된 댓글에는 답글을 작성할 수 없음"),
+    COMMENT_NOT_FOUND_ERROR(404, "C004", "댓글을 찾을 수 없음"),
+    COMMENT_DELETED_ERROR(404, "C005", "삭제된 댓글입니다"),
+
+    //Record
+    RECORD_NOT_FOUND_ERROR(400, "R001", "기록을 찾을 수 없음");
 
     private final int status;
     private final String code;
