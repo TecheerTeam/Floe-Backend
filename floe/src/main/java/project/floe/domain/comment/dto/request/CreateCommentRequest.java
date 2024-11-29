08d1c54e091186f4ebe9cbe949aa37c74b886df3
kyungsoo <yksoo00@naver.com>
2024-11-30 06:17:41 +0900

    feat(CreateCommentRequest) : 댓글 내용 사이즈 조건 추가

--- floe/src/main/java/project/floe/domain/comment/dto/request/CreateCommentRequest.java
+++ floe/src/main/java/project/floe/domain/comment/dto/request/CreateCommentRequest.java
@@ -1,7 +1,7 @@
 package project.floe.domain.comment.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,9 +20,9 @@ public class CreateCommentRequest {
     @NotNull(message = "사용자 ID는 필수입니다.")
     private Long userId;
 
-    @NotBlank(message = "댓글 내용은 필수입니다.")
+    @Size(min = 1, max = 200, message = "댓글 내용은 1자 이상 200자 이하로 작성해주세요.")
     private String content;
 
-    private Long parentId; // 부모 댓글 ID (답글인 경우 null 가능)
+    private Long parentId;
 
 }
