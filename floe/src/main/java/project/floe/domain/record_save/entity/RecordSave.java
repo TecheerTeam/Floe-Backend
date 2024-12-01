dfbfe78619b162c0324de76dde7ea2f8859ff5f7
박진형 <skill1218@naver.com>
2024-12-01 15:04:56 +0900

    feat: record_save entity 구현

---
+++ floe/src/main/java/project/floe/domain/record_save/entity/RecordSave.java
@@ -0,0 +1,42 @@
+package project.floe.domain.record_save.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import project.floe.domain.record.entity.Record;
+import project.floe.domain.user.entity.User;
+import project.floe.entity.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "record_id"})
+})
+@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE record_save SET is_deleted = true WHERE id = ?")
+public class RecordSave extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "record_id", nullable = false)
+    private Record record;
+}
