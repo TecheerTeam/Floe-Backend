93b64fafb59ecdaa8732e418e186c2b6cea1d683
kyungsoo <yksoo00@naver.com>
2024-12-13 01:26:28 +0900

    feat(CommentLikeJpaRepository): 댓글 좋아요 레포지토리 추가 (유저 목록 가져올때 serivce가 아닌 @query 사용하여 유저 목록에 필요한 값 가저옴)

---
+++ floe/src/main/java/project/floe/domain/comment_like/respository/CommentLikeJpaRepository.java
@@ -0,0 +1,21 @@
+package project.floe.domain.comment_like.respository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import project.floe.domain.comment_like.dto.response.GetCommentLikeUserResponse;
+import project.floe.domain.comment_like.entity.CommentLike;
+
+@Repository
+public interface CommentLikeJpaRepository extends JpaRepository<CommentLike, Long> {
+    long countByCommentId(Long commentId);
+
+    int deleteByCommentIdAndUserId(Long commentId, Long userId);
+
+    @Query("SELECT new project.floe.domain.comment_like.dto.response.GetCommentLikeUserResponse(u.id, u.nickname, u.profileImage) "
+            +
+            "FROM CommentLike cl JOIN cl.user u WHERE cl.comment.id = :commentId")
+    List<GetCommentLikeUserResponse> findUserListByCommentId(@Param("commentId") Long commentId);
+}
