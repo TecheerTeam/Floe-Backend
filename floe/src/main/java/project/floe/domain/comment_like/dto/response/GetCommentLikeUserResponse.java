4690831490bf17ad923214251dea0101e2fe7335
kyungsoo <yksoo00@naver.com>
2024-12-13 02:07:23 +0900

    fix(GetCommentLikeUserResponse): username -> nickname으로 수정

--- floe/src/main/java/project/floe/domain/comment_like/dto/response/GetCommentLikeUserResponse.java
+++ floe/src/main/java/project/floe/domain/comment_like/dto/response/GetCommentLikeUserResponse.java
@@ -6,12 +6,12 @@ import project.floe.domain.user.entity.User;
 @Getter
 public class GetCommentLikeUserResponse {
     private final Long userId;
-    private final String userName;
+    private final String NickName;
     private final String profileImage;
 
     public GetCommentLikeUserResponse(User user) {
         this.userId = user.getId();
-        this.userName = user.getNickname();
+        this.NickName = user.getNickname();
         this.profileImage = user.getProfileImage();
     }
 }
