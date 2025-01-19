b0d39d3b59e8eba6167146aa054fc841a6868c3f
박진형 <skill1218@naver.com>
2025-01-20 01:03:22 +0900

    fix: record-like extends baseEntity

--- floe/src/main/java/project/floe/domain/record_like/entity/RecordLike.java
+++ floe/src/main/java/project/floe/domain/record_like/entity/RecordLike.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import project.floe.domain.record.entity.Record;
 import project.floe.domain.user.entity.User;
+import project.floe.entity.BaseEntity;
 
 @Getter
 @Entity
@@ -26,7 +27,7 @@ import project.floe.domain.user.entity.User;
                 )
         }
 )
-public class RecordLike {
+public class RecordLike extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
