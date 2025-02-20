efa1fb2d6a7044cde123afa1b89ab8ece80ca154
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-02-20 12:11:51 +0900

    fix: 일단 리다이렉트 하도록만 변경

--- floe/src/main/java/project/floe/global/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ floe/src/main/java/project/floe/global/auth/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -47,5 +47,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
+        response.sendRedirect("http://localhost:3000/home");
     }
 }
